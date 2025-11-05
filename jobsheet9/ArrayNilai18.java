d1f69c29c6c666f4dfadfe7b3efe045f1369d492
“1NabilAkbar1” <nabilinformatika1@gmail.com>
2025-11-05 11:44:22 +0700

    Percobaan 2.2 (revisi)

--- jobsheet9/ArrayNilai18.java
+++ jobsheet9/ArrayNilai18.java
@@ -1,6 +1,6 @@
     package jobsheet9;
     import java.util.Scanner;
-    public class ArrayNilai {
+    public class ArrayNilai18 {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int[] nilaiAkhir = new int[10];
